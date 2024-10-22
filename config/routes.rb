aec9a4d30c79cc738bd97fd4184894432d8818b3
Noah Yanga <noag29@hotmail.com>
2024-10-21 19:29:41 -0500

    update show page and route to display individual prodcuts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "products/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,4 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get '/product', to: 'products#index'
+  get '/product/:id', to: 'products#show'
 end
