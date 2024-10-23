1d17f7d3e9c914aee84b3a0a1ffba8fd81c5680a
Noah Yanga <noag29@hotmail.com>
2024-10-22 23:16:10 -0500

    populate products into database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,41 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-require 'faker'
+# require 'faker'
 
-676.times do
+# 676.times do
+#   Product.create!(
+#     title: Faker::Commerce.product_name,
+#     price: Faker::Commerce.price(range: 0.1..100.0),
+#     stock_quantity: Faker::Number.between(from: 1, to: 100)
+#   )
+# end
+
+# puts "Seeded 676 products"
+
+require 'csv'
+
+# Product.destroy_all
+# Category.destroy_all
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+
+products.each do |product_row|
+  category_name = product_row['category']
+  
+  category = Category.find_or_create_by!(name: category_name)
+
   Product.create!(
-    title: Faker::Commerce.product_name,
-    price: Faker::Commerce.price(range: 0.1..100.0),
-    stock_quantity: Faker::Number.between(from: 1, to: 100)
+    title: product_row['name'], 
+    price: product_row['price'].to_f,
+    description: product_row['description'], 
+    stock_quantity: product_row['stock quantity'].to_i,
+    category: category 
   )
 end
 
-puts "Seeded 676 products"
+puts "#{Product.count} products created"
